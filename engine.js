f621e85df5b5a22f44ae7a85af69103ff66de5f0
Luís Rêgo <24704807+LuisSousaRego@users.noreply.github.com@users.noreply.github.com>
2026-02-07 14:02:43 +0100

    js scaffold

--- engine.js
+++ engine.js
@@ -1,1 +1,35 @@
+function writeUserMessage(message) {
 
+}
+
+function writeAiMessage(message) {
+
+}
+
+function addResponseOption(responses) {
+
+}
+
+function clearResponseOptions() {
+
+}
+
+function advanceStory(userMessage, nextKey) {
+    writeUserMessage(userMessage)
+
+    const nextStep = game[nextKey]
+
+    for (const m of nextStep.messages) {
+        writeAiMessage(m)
+    }
+
+    clearResponseOptions()
+
+    for (const r of nextStep.responses) {
+        addResponseOption(r)
+    }
+}
+
+
+// start game
+advanceStory("", "start")
